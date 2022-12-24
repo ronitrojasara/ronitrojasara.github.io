4da6eb029c0add29ab2078d6349140007bee4443
Ronit Rojasara <84675961+ronitrojasara@users.noreply.github.com>
2022-12-24 18:37:52 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,11 +3,7 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  'index.html',
-  './', // Alias for index.html
-  'styles.css',
-  '../../styles/main.css',
-  'demo.js'
+  'index_x.html'
 ];
 
 // The install handler takes care of precaching the resources we always need.
