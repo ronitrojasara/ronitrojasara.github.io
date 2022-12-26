6c5a04346f2adc63c3c95ac6938da15955d1de42
Ronit Rojasara <84675961+ronitrojasara@users.noreply.github.com>
2022-12-26 12:36:20 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -53,18 +53,4 @@ self.addEventListener('fetch', event => {
     );
   }
 });
-let deferredPrompt;
-    window.addEventListener('beforeinstallprompt', (e) => {
-        deferredPrompt = e;
-    });
 
-    const installApp = document.getElementById('installApp');
-    installApp.addEventListener('click', async () => {
-        if (deferredPrompt !== null) {
-            deferredPrompt.prompt();
-            const { outcome } = await deferredPrompt.userChoice;
-            if (outcome === 'accepted') {
-                deferredPrompt = null;
-            }
-        }
-    });
